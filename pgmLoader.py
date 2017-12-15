ddc20a6e848f2ebafbfc318d33ed6b6aee2bbf23
Unknown <lizardswimmer@gmail.com>
2017-12-15 11:23:09 -0800

    pgm work

--- pgmLoader.py
+++ pgmLoader.py
@@ -1,9 +1,21 @@
 import os
 import sys
 
-file = "range.pgm"
+file = "bird.pgm"
 
-with open(file, "rb") as f:
+whitespace = [" ", "\n", "\t", "\r"]
+
+with open(file, "r") as f:
 	raw = f.read()
 
-print(raw.decode())
+assert raw[:2] == "P2", "Not ASCII grayscale PGM file"
+lines = [x for x in raw.split("\n") if "#" not in x[:1]]
+width, height = lines[1].split(" ")
+width = int(width)
+height = int(height)
+maxval = lines[2]
+img = [x for x in lines[3:] if x is not ""]
+lsb = "".join([bin(int(x))[-1] for x in img])
+lsb_chunks = [lsb[x*8:(x+1)*8] for x in range(int(len(lsb)/8))]
+chars = [chr(int(x, 2)) for x in lsb_chunks]
+print(chars)
